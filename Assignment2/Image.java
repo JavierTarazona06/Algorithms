5eb4d5ec582042de02d264303e6311ab8ef3a37a
Javier Tarazona J. <developer@engin.ai>
2024-10-04 17:46:13 -0600

    Asss2

--- Assignment2/Image.java
+++ Assignment2/Image.java
@@ -12,9 +12,30 @@ public class Image{
         int x = scan.nextInt();
         //Do not edit anything above
     
-        //write the NSIDs of the members of your group here: 
+        //write the NSIDs of the members of your group here:
+        // elr490
         //Write your code below
         //BEGIN
+        int[][] dp = new int[21][3];
+
+        dp[0][0] = 1;
+        dp[0][1] = 1;
+        dp[0][2] = 1;
+
+        for (int m = 1; m < 21; m++){
+            for (int n=2; n > -1; n--){
+                if (n == 2){
+                    dp[m][n] = dp[m-1][1] + dp[m-1][2];
+                } else if (n == 1){
+                    dp[m][n] = dp[m-1][0] + dp[m][2];
+                } else {
+                    dp[m][n] = dp[m][1];
+                }
+
+            }
+        }
+
+        System.out.println(dp[x][0]);
 
         //END
         //Do not edit anything below 
